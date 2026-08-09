d85198e869614c1fca8d707ad776aca581e0bd83
Ch.V.V.Satyanarayana <satyanarayanachittala4@gmail.com>
2026-08-09 22:17:14 +0530

    Time: 91 ms (73.82%) | Memory: 25.7 MB (59.78%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def stoneGameII(self, piles):
+        n = len(piles)
+
+        # Suffix sum: suffix[i] = stones from i to n-1
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        # dp(i, M) = maximum stones current player can get
+        # starting from index i with M
+        memo = {}
+
+        def dp(i, M):
+            if i >= n:
+                return 0
+
+            if (i, M) in memo:
+                return memo[(i, M)]
+
+            # If we can take all remaining piles
+            if i + 2 * M >= n:
+                memo[(i, M)] = suffix[i]
+                return suffix[i]
+
+            best = 0
+
+            # Try taking X piles, where 1 <= X <= 2*M
+            for X in range(1, 2 * M + 1):
+                # Current player gets suffix[i] - opponent's maximum
+                opponent = dp(i + X, max(M, X))
+                current = suffix[i] - opponent
+
+                best = max(best, current)
+
+            memo[(i, M)] = best
+            return best
+
+        return dp(0, 1)
